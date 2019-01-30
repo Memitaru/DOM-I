7919a6752f05f862e819c376cec1faafd9c1c4da
Memitaru <amandatwah@gmail.com>
2019-01-29 21:26:37 -0800

    Fixed all h4s

--- js/index.js
+++ js/index.js
@@ -61,9 +61,19 @@ document.querySelector('button').textContent = siteContent['cta']['button'];
 
 // Main Content
 
+let h4 = document.querySelectorAll('h4');
+h4[0].textContent = siteContent['main-content']['features-h4'];
+h4[1].textContent = siteContent['main-content']['about-h4']
 
+document.querySelector('#middle-img').setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+h4[2].textContent = siteContent['main-content']['services-h4'];
+h4[3].textContent = siteContent['main-content']['product-h4'];
+h4[4].textContent = siteContent['main-content']['vision-h4'];
 
 // Contact
+
+h4[5].textContent = siteContent['contact']['contact-h4'];
 
 // Footer
 
